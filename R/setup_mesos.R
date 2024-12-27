70b918e0962b21ac9869ec9e0c48449e3c344235
Stephan Daus <stephus.daus@gmail.com>
2024-12-27 18:46:55 +0100

    Rewrote setup_mesos for using metadata.yml

--- R/setup_mesos.R
+++ R/setup_mesos.R
@@ -1,24 +1,3 @@
-set_underscore_on_filenames <- function(files_to_process) {
-    if (!is.character(files_to_process) ||
-        length(files_to_process) == 0 ||
-        !all(file.exists(files_to_process))) {
-        cli::cli_abort("{.arg files_to_process} must be a character vector of paths to existing files, not {.obj_type_friendly {files_to_process}}: {files_to_process}.")
-    }
-
-
-
-    files_to_be_renamed <-
-        rlang::set_names(stringi::stri_replace_last_fixed(files_to_process, pattern = "/", replacement = "/_"),
-            nm = files_to_process
-        )
-    file.rename(
-        from = names(files_to_be_renamed),
-        to = unname(files_to_be_renamed)
-    )
-    files_to_be_renamed
-}
-
-
 search_and_replace_files <- function(
     files,
     pattern,
@@ -28,6 +7,7 @@ search_and_replace_files <- function(
         length(pattern) != length(replacement)) {
         cli::cli_abort("{.arg pattern} and {.arg replacement} must be character vectors of same length.")
     }
+
     files |>
         unname() |>
         lapply(FUN = function(.x) {
@@ -38,114 +18,122 @@ search_and_replace_files <- function(
     files
 }
 
-create_mesos_stubs_from_main_files <- function(
-    files,
-    mesos_var,
-    mesos_groups,
-    qmd_regex = "\\.qmd",
-    main_files = c("index", "report")) {
-    ####
+create_mesos_stubs_from_main_files <- function(mesos_df,
+                                               main_directory,
+                                               files_to_process) {
+    for (j in seq_len(length(mesos_df))) {
+        mesos_var <- names(mesos_df[[j]])[1]
+        mesos_var_pretty <- unname(get_raw_labels(mesos_df[[j]], col_pos = 1))
+        if (is.null(mesos_var_pretty)) mesos_var_pretty <- mesos_var
+        mesos_groups_pretty <- as.character(mesos_df[[j]][[1]])
+        mesos_groups_pretty <- mesos_groups_pretty[!is.na(mesos_groups_pretty)]
+        mesos_groups_abbr <- filename_sanitizer(mesos_groups_pretty, max_chars = 12, accept_hyphen = TRUE, make_unique = TRUE)
+        mesos_groups_base_paths <- fs::path(main_directory, mesos_var, mesos_groups_abbr)
+        fs::dir_create(fs::path(main_directory, mesos_var))
+        fs::dir_create(mesos_groups_base_paths)
 
-    if (!is.character(files) || !any(file.exists(files))) {
-        cli::cli_warn("No files found.")
-        return()
-    }
-    if (!rlang::is_string(mesos_var) || !is.character(mesos_groups)) {
-        cli::cli_abort("{.arg mesos_var} must be a string and {.arg mesos_groups} must be a character vector.")
-    }
 
-    dir_path <- unique(dirname(files))
-    if (length(dir_path) > 1) cli::cli_abort("All files must be in the same folder.")
-
-    new_qmd_files <-
-        files |>
-        stringi::stri_replace_last_fixed(pattern = qmd_regex, replacement = "") |>
-        basename() |>
-        tidyr::expand_grid(
-            main_file = _,
-            mesos_group = mesos_groups
-        ) |>
-        dplyr::rowwise() |>
-        dplyr::mutate(
-            main_file_no_ = stringi::stri_replace_first_regex(.data$main_file,
-                pattern = "^_|\\.[r]qmd", replacement = ""
-            ),
-            new_file_path = fs::path(
-                .env$dir_path,
-                .data$mesos_group,
-                .data$main_file_no_
-            ),
-            contents = {
-                yaml <- list(
-                    params = list(
-                        mesos_var = .env$mesos_var,
-                        mesos_group = .data$mesos_group
-                    )
-                )
-                if (.data$main_file_no_ %in% main_files) {
-                    yaml$title <- paste0(.data$mesos_group)
-                }
-                yaml <- yaml::as.yaml(x = yaml)
-                paste0("---\n", yaml, "---\n", paste0(
-                    "\n{{< include ../",
-                    .data$main_file, " >}}\n"
-                ), sep = "\n")
-            }
+        # Write _metadata.yml in each mesos_var folder
+        yaml::write_yaml(
+            x = list(params = list(
+                mesos_var = mesos_var,
+                mesos_var_pretty = mesos_var_pretty
+            )),
+            file = fs::path(main_directory, mesos_var, "_metadata.yml")
         )
 
+        for (f in seq_along(files_to_process)) {
+            cat(paste0('{{< include "../../', basename(unname(files_to_process[f])), '" >}}'),
+                file = fs::path(main_directory, mesos_var, basename(unname(files_to_process[f])))
+            )
+        }
 
-    fs::dir_create(dirname(new_qmd_files$new_file_path), recurse = TRUE)
-    for (i in seq_len(nrow(new_qmd_files))) {
-        cat(new_qmd_files[i, "contents", drop = TRUE], file = new_qmd_files[i,
-            "new_file_path",
-            drop = TRUE
-        ])
+
+        for (i in seq_along(mesos_groups_pretty)) {
+            mesos_group <- mesos_groups_pretty[i]
+
+            # Write _metadata.yml in each mesos_group foldre
+            yaml::write_yaml(
+                x = list(params = list(mesos_group = mesos_group)),
+                file = fs::path(mesos_groups_base_paths[i], "_metadata.yml")
+            )
+            # Write chapter stubs in each mesos_group folder
+            out_files <-
+                fs::path(
+                    mesos_groups_base_paths[i],
+                    stringi::stri_replace_first_regex(basename(unname(files_to_process)),
+                        pattern = "^_",
+                        replacement = ""
+                    )
+                )
+
+            for (k in seq_along(unname(files_to_process))) {
+                cat(paste0('{{< include "../', basename(unname(files_to_process[k])), '" >}}'),
+                    file = out_files[k]
+                )
+            }
+        }
     }
-    new_qmd_files
 }
 
 
-#' Simply create qmd-files for mesos reports
+#' Simply create qmd-files and yml-files for mesos reports
+#' @param main_directory String, path to where the _metadata.yml, stub QMD-files and their subfolders are created.
 #' @param files_to_process Character vector of files used as templates for the mesos stubs.
-#' @param mesos_var String, variable used during rendering with saros::makeme() to obtain the column in the dataset which it will filter on.
-#' @param mesos_groups Character vector for which stub-qmd files will be made
+#' @param mesos_df List of single-column data frames where each variable is a mesos variable, optionally with a variable label indicating its pretty name. The values in each variable are the mesos groups. NA is silently ignored.
 #' @param main_files Character vector of files for which titles should be set as the mesos_group. Optional but recommended.
 #' @param read_syntax_pattern,read_syntax_replacement Optional strings, any regex pattern to search and replace in the qmd-files. If NULL, will ignore it.
 #' @param qmd_regex String. Experimental feature for allowing Rmarkdown, not yet tested.
 #' @export
 setup_mesos <- function(
+    main_directory,
     files_to_process,
-    mesos_var,
-    mesos_groups,
+    mesos_df,
     main_files = c("index", "report"),
     read_syntax_pattern = "qs::qread\\('",
     read_syntax_replacement = "qs::qread('../",
     qmd_regex = "\\.qmd") {
     ## Checks
 
+    if (!inherits(main_directory, "character") || length(main_directory) == 0) cli::cli_abort("{.arg main_directory} must be a string, not {.obj_type_friendly {main_directory}}")
+    if (!is.character(files_to_process) ||
+        length(files_to_process) == 0 ||
+        !all(file.exists(files_to_process))) {
+        cli::cli_abort("{.arg files_to_process} must be a character vector of paths to existing files, not {.obj_type_friendly {files_to_process}}: {files_to_process}.")
+    }
+    check_files_to_process <- stringi::stri_extract_last_regex(files_to_process, pattern = "/[^_/\\\\]+$")
+    check_files_to_process <- check_files_to_process[!is.na(check_files_to_process)]
+    if (length(check_files_to_process)) {
+        cli::cli_warn(c(
+            "{.arg files_to_process} are expected to have filenames starting with underscore for most mesos setups. These are not:",
+            "{check_files_to_process}"
+        ))
+    }
+    if (!is.list(mesos_df)) cli::cli_abort("{.arg mesos_df} must be a list of single-column data.frames, not {.obj_type_friendly {mesos_df}}")
+    if (!inherits(main_files, "character")) cli::cli_abort("{.arg main_files} must be a character vector, not a {.obj_type_friendly {main_files}}")
+    if (!inherits(read_syntax_pattern, "character")) cli::cli_abort("{.arg read_syntax_pattern} must be a string (regex), not a {.obj_type_friendly {read_syntax_pattern}}")
+    if (!inherits(read_syntax_replacement, "character")) cli::cli_abort("{.arg read_syntax_replacement} must be a string (regex), not a {.obj_type_friendly {read_syntax_replacement}}")
+    if (!inherits(qmd_regex, "character") || length(qmd_regex) == 0) cli::cli_abort("{.arg qmd_regex} must be a string, not a {.obj_type_friendly {qmd_regex}}")
 
-    files_to_be_renamed <-
-        set_underscore_on_filenames(files_to_process = files_to_process)
+
+    create_mesos_stubs_from_main_files(
+        mesos_df = mesos_df,
+        main_directory = main_directory,
+        files_to_process = files_to_process
+    )
 
     if (is.character(read_syntax_pattern) &&
         is.character(read_syntax_replacement)) {
         search_and_replace_files(
-            files = unname(files_to_be_renamed),
-            pattern = read_syntax_pattern, replacement = read_syntax_replacement
+            files = files_to_process,
+            pattern = read_syntax_pattern,
+            replacement = read_syntax_replacement
         )
     }
 
-    mesos_files <-
-        create_mesos_stubs_from_main_files(
-            files = unname(files_to_be_renamed),
-            mesos_var = mesos_var,
-            mesos_groups = mesos_groups,
-            main_files = main_files,
-            qmd_regex = qmd_regex
-        )
+    cli::cli_inform("Successfully made files!")
 
     list(
-        files_renamed = files_to_be_renamed,
-        mesos_subfolder_files = mesos_files
+        files_renamed = files_to_process
     )
 }
