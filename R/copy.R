134a0f1b222de958281aa9edce45ece86bf161ee
Stephan Daus <stephus.daus@gmail.com>
2025-10-21 22:49:41 +0200

    refactor: remove unused code and commented-out functions
    - Remove commented-out old implementations in attach_new_output_to_output.R
    - Remove commented-out find_test() and find_stat_config() functions
    - Remove unused internal functions: compare_many(), copy_content()
    - Remove entire check_paths.R file (fully commented out)
    - Remove render_full_reports.R and its documentation
    - Remove commented line in create_r_files.R
    - Remove commented validation in create_email_credentials.R
    
    This removes ~150 lines of dead code with no functional impact.

--- R/copy.R
+++ R/copy.R
@@ -1,4 +1,3 @@
-
 # Function to recursively copy folders
 recursive_copy <- function(from, to) {
   # Create the directory structure in the destination
@@ -14,31 +13,6 @@ recursive_copy <- function(from, to) {
     } else {
       # If it's a file, copy it to the destination
       file.copy(file, to)
-    }
-  }
-}
-
-copy_content <- function(from_files, from_folders, to_folder) {
-  # Create the destination directory if it doesn't exist
-  if (!dir.exists(to_folder)) {
-    dir.create(to_folder, recursive = TRUE, showWarnings = TRUE)
-  } else {
-    # If the directory exists, clear its contents
-    file.remove(list.files(to_folder, full.names = TRUE))
-  }
-
-
-  # Copy each file to the destination folder
-  for (file in from_files) {
-    if (file.exists(file)) {
-      file.copy(file, to_folder)
-    }
-  }
-
-  # Copy each folder to the destination folder
-  for (folder in from_folders) {
-    if (dir.exists(folder)) {
-      recursive_copy(folder, file.path(to_folder, basename(folder)))
     }
   }
 }
