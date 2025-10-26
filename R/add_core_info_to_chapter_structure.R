ef4de30a6682a9d19b9e3bd570aebb1b3c6deec5
Stephan Daus <stephus.daus@gmail.com>
2025-10-26 09:07:37 +0100

    Fix: Handle variable names with spaces (GH #151)
    Preserve spaces in backtick-quoted variable names during processing.
    The delimiter regex was splitting variable names at spaces, even when
    they were within backticks.
    
    Changes:
    - Modified add_core_info_to_chapter_structure to preserve spaces within
      backtick-quoted variable names before splitting on commas
    - Uses placeholder replacement strategy to temporarily protect
      backtick-quoted sections from delimiter processing
    - Added test to verify variable names with spaces are handled correctly
    
    Closes #151

--- R/add_core_info_to_chapter_structure.R
+++ R/add_core_info_to_chapter_structure.R
@@ -1,48 +1,89 @@
 add_core_info_to_chapter_structure <-
   function(chapter_structure) {
-
     col_headers <- c("dep", "indep")
-    delim_regex <- "[,[:space:]]+"
-    attr(delim_regex, "options") <-
-      list(case_insensitive = FALSE,
-           comments = FALSE,
-           dotall = FALSE,
-           multiline = FALSE)
-    class(delim_regex) <- c("stringr_regex", "stringr_pattern", "character")
 
     out <-
       tidyr::pivot_longer(chapter_structure,
-                          cols = tidyselect::any_of(col_headers),
-                          values_to = ".variable_selection")
+        cols = tidyselect::any_of(col_headers),
+        values_to = ".variable_selection"
+      )
     out <-
-      vctrs::vec_slice(out,
-                       !(out$name == "indep" &
-                           (is.na(out$.variable_selection) |
-                              out$.variable_selection == "")))
+      vctrs::vec_slice(
+        out,
+        !(out$name == "indep" &
+          (is.na(out$.variable_selection) |
+            out$.variable_selection == ""))
+      )
+
+    # Split .variable_selection but preserve spaces within backtick-quoted names
+    # First, normalize the delimiters outside of backticks
+    out$.variable_selection <- vapply(
+      out$.variable_selection,
+      function(str) {
+        if (is.na(str) || str == "") {
+          return(str)
+        }
+
+        # Find all backtick-quoted variable names
+        backtick_pattern <- "`[^`]+`"
+        matches <- stringi::stri_extract_all_regex(str, backtick_pattern)[[1]]
+
+        if (all(is.na(matches))) {
+          # No backtick-quoted names, process normally
+          stringi::stri_replace_all_regex(str, "[,[:space:]]+", ",")
+        } else {
+          # Replace backtick sections with placeholders, process, then restore
+          placeholders <- paste0("__BQUOTE_", seq_along(matches), "__")
+          temp_str <- str
+          for (i in seq_along(matches)) {
+            temp_str <- stringi::stri_replace_first_fixed(temp_str, matches[i], placeholders[i])
+          }
+          # Now replace spaces/commas in the non-backtick parts
+          temp_str <- stringi::stri_replace_all_regex(temp_str, "[,[:space:]]+", ",")
+          # Restore backtick-quoted names
+          for (i in seq_along(matches)) {
+            temp_str <- stringi::stri_replace_first_fixed(temp_str, placeholders[i], matches[i])
+          }
+          temp_str
+        }
+      },
+      character(1),
+      USE.NAMES = FALSE
+    )
+
+    # Now split by comma (which won't be inside backticks after our processing)
     out <-
       tidyr::separate_longer_delim(out,
-                                   cols = ".variable_selection",
-                                   delim = delim_regex)
+        cols = ".variable_selection",
+        delim = ","
+      )
     out <-
       tidyr::separate(out,
-                      col = .data$name,
-                      into = ".variable_role",
-                      sep="_")
+        col = .data$name,
+        into = ".variable_role",
+        sep = "_"
+      )
 
     out[[".variable_role"]] <-
       ifelse(is.na(out[[".variable_selection"]]) |
-               out[[".variable_selection"]] == "", NA_character_, out[[".variable_role"]])
+        out[[".variable_selection"]] == "", NA_character_, out[[".variable_role"]])
 
     out[[".variable_selection"]] <-
-      dplyr::if_else(!stringi::stri_detect(out$.variable_selection,
-                                           regex = "matches\\(") &
-                       stringi::stri_detect(out$.variable_selection,
-                                            regex = "\\*"),
-                     true = stringi::stri_c(ignore_null=TRUE,
-                                            "matches('",
-                                            out$.variable_selection,
-                                            "')"),
-                     false = out$.variable_selection)
+      dplyr::if_else(
+        !stringi::stri_detect(out$.variable_selection,
+          regex = "matches\\("
+        ) &
+          stringi::stri_detect(out$.variable_selection,
+            regex = "\\*"
+          ),
+        true = stringi::stri_c(
+          ignore_null = TRUE,
+          "matches('",
+          out$.variable_selection,
+          "')"
+        ),
+        false = out$.variable_selection
+      )
     out <-
       dplyr::distinct(out, .keep_all = TRUE)
     out <-
